41c062523220120d176ffcfe5feaab621b95e210
coyotewind <coyoteswind@gmail.com>
2020-11-28 14:38:06 -0500

    push to dev ready to deploy

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -15,7 +15,7 @@ function MenuBar() {
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
     const menuBar = user ? (
-        <Menu pointing secondary size='massive' color='teal'>
+        <Menu pointing secondary size='massive' color='blue'>
             <Menu.Item
                     name='home'
                     active={activeItem === 'home'}
@@ -36,7 +36,7 @@ function MenuBar() {
             </Menu.Menu>
         </Menu>
     ) : (
-        <Menu pointing secondary size='massive' color='teal'>
+        <Menu pointing secondary size='massive' color='blue'>
             <Menu.Item
                 name='home'
                 active={activeItem === 'home'}
